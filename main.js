1a8faec6572246b290e0b10820f66f751d6d18dc
JahanSurti <126305241+JahanSurti@users.noreply.github.com>
2023-08-15 18:32:30 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+img = "";
+status = "";
+object = "";
+function preload() {
+    img = loadImage("Bedroom.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectdetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectdetector.detect(img, gotResult);
+  }
+
+  function gotResult(error, results) {
+    if (error) {
+     console.log(error);
+    }
+     console.log(results);
+     object = results;
+ }
+
+ function draw()
+{
+    image(img, 0, 0, 640, 420);
+    if(status != "")
+    {
+        for(i = 0; i < object.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+
+    }
+    
+}
